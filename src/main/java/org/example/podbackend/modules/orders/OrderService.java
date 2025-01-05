817664af429a765b0a374ae0c4b79113b917048c
quangtho908 <quangtho23062002@gmail.com>
2025-01-05 18:24:23 +0700

    refactor code

--- src/main/java/org/example/podbackend/modules/orders/OrderService.java
+++ src/main/java/org/example/podbackend/modules/orders/OrderService.java
@@ -1,6 +1,7 @@
 package org.example.podbackend.modules.orders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
 import org.example.podbackend.Security.Models.PodUserDetail;
 import org.example.podbackend.common.enums.StatusOrder;
 import org.example.podbackend.common.exceptions.BadRequestException;
@@ -18,6 +19,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
@@ -30,168 +34,168 @@ import java.util.concurrent.Executor;
 
 @Service
 public class OrderService {
-  private final InProgressOrderRepository inProgressOrderRepository;
-  private final ProductOrderRepository productOrderRepository;
-  private final ProductRepository productRepository;
-  private final MerchantRepository merchantRepository;
-  private final UserRepository userRepository;
-  private final TableRepository tableRepository;
-  private final ModelMapper modelMapper;
-  private final Executor asyncExecutor;
-  private final ObjectMapper objectMapper;
-  private final OrderMapper orderMapper;
-  private final NotificationService notificationService;
-
-  public OrderService(
-          InProgressOrderRepository inProgressOrderRepository,
-          ProductOrderRepository productOrderRepository,
-          ProductRepository productRepository,
-          MerchantRepository merchantRepository,
-          UserRepository userRepository,
-          TableRepository tableRepository,
-          ModelMapper modelMapper,
-          Executor asyncExecutor,
-          ObjectMapper objectMapper,
-          OrderMapper orderMapper,
-          NotificationService notificationService) {
-    this.inProgressOrderRepository = inProgressOrderRepository;
-    this.productOrderRepository = productOrderRepository;
-    this.productRepository = productRepository;
-    this.merchantRepository = merchantRepository;
-    this.userRepository = userRepository;
-    this.tableRepository = tableRepository;
-    this.modelMapper = modelMapper;
-    this.asyncExecutor = asyncExecutor;
-    this.objectMapper = objectMapper;
-    this.orderMapper = orderMapper;
-    this.notificationService = notificationService;
-  }
-
-  public ResponseEntity<?> filter(@RequestParam Map<String, String> allParam) {
-    FilterOrderDTO dto = objectMapper.convertValue(allParam, FilterOrderDTO.class);
-    if(dto.getId() != null) {
-      InProgressOrder inProgressOrder = this.inProgressOrderRepository.findByIdAndMerchantId(dto.getId(), dto.getMerchantId());
-      if(inProgressOrder == null) throw new BadRequestException("Order does not exist");
-      return ResponseEntity.ok(orderMapper.mapToResponse(inProgressOrder));
+    private final InProgressOrderRepository inProgressOrderRepository;
+    private final ProductOrderRepository productOrderRepository;
+    private final ProductRepository productRepository;
+    private final MerchantRepository merchantRepository;
+    private final UserRepository userRepository;
+    private final TableRepository tableRepository;
+    private final ModelMapper modelMapper;
+    private final Executor asyncExecutor;
+    private final ObjectMapper objectMapper;
+    private final OrderMapper orderMapper;
+    private final NotificationService notificationService;
+    private final ProductOrderService productOrderService;
+    public OrderService(
+            InProgressOrderRepository inProgressOrderRepository,
+            ProductOrderRepository productOrderRepository,
+            ProductRepository productRepository,
+            MerchantRepository merchantRepository,
+            UserRepository userRepository,
+            TableRepository tableRepository,
+            ModelMapper modelMapper,
+            Executor asyncExecutor,
+            ObjectMapper objectMapper,
+            OrderMapper orderMapper,
+            NotificationService notificationService,
+            ProductOrderService productOrderService
+    ) {
+      this.inProgressOrderRepository = inProgressOrderRepository;
+      this.productOrderRepository = productOrderRepository;
+      this.productRepository = productRepository;
+      this.merchantRepository = merchantRepository;
+      this.userRepository = userRepository;
+      this.tableRepository = tableRepository;
+      this.modelMapper = modelMapper;
+      this.asyncExecutor = asyncExecutor;
+      this.objectMapper = objectMapper;
+      this.orderMapper = orderMapper;
+      this.notificationService = notificationService;
+      this.productOrderService = productOrderService;
     }
-    List<InProgressOrder> inProgressOrders = this.inProgressOrderRepository.filter(
-            dto.getMerchantId(),
-            dto.getStatuses(),
-            dto.getFromDate(),
-            dto.getToDate(),
-            PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "createdAt"))
-    );
-    List<OrderResponse> response = orderMapper.mapToResponse(inProgressOrders);
-    return ResponseEntity.ok(response);
-  }
 
-  public ResponseEntity<OrderResponse> createOrder(CreateOrderDTO dto) throws ExecutionException, InterruptedException {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    PodUserDetail userDetails = (PodUserDetail) authentication.getPrincipal();
-    CompletableFuture<Merchant> merchantRead = CompletableFuture.supplyAsync(() ->
-            merchantRepository.findByIdAndUserId(dto.getMerchantId(), userDetails.getId()), asyncExecutor
-    );
-    CompletableFuture<Users> userRead = CompletableFuture.supplyAsync(() ->
-      userRepository.findById(userDetails.getId()).orElse(null),
-      asyncExecutor
-    );
-    CompletableFuture<Tables> tableRead = CompletableFuture.supplyAsync(() ->
-      dto.getTableId() == null ? null : tableRepository.findByIdAndMerchantIdAndIsUsedIsFalse(dto.getTableId(), dto.getMerchantId()), asyncExecutor
-    );
-    CompletableFuture<Void> allTasks = CompletableFuture.allOf(merchantRead, userRead, tableRead);
-    allTasks.join();
-
-    Merchant merchant = merchantRead.get();
-    if(merchant == null) throw new BadRequestException("Merchant not found");
-    Users user = userRead.get();
-    if(user == null) throw new BadRequestException("User not found");
-    Tables table = tableRead.get();
-
-    InProgressOrder inProgressOrder = modelMapper.map(dto, InProgressOrder.class);
-    inProgressOrder.setMerchant(merchant);
-    if(table != null)  inProgressOrder.setTables(table);;
-    inProgressOrder.setCreatedBy(user);
-
-    InProgressOrder savedInProgressOrder = inProgressOrderRepository.save(inProgressOrder);
-
-    CompletableFuture<Void> threads = CompletableFuture.allOf(dto.getProducts().stream().map((productOrderDTO) ->
-      CompletableFuture.runAsync(() -> {
-        Product product = this.productRepository.findByIdAndMerchantId(productOrderDTO.getProductId(), dto.getMerchantId());
-        if (product == null) return;
-        ProductOrder productOrder = modelMapper.map(productOrderDTO, ProductOrder.class);
-        productOrder.setInProgressOrder(savedInProgressOrder);
-        productOrder.setProduct(product);
-        this.productOrderRepository.save(productOrder);
-      }, asyncExecutor)
-    ).toArray(CompletableFuture[]::new));
-    threads.join();
-    if(table != null ) {
-      table.setUsed(true);
-      this.tableRepository.save(table);
+    public ResponseEntity<?> filter(@RequestParam Map<String, String> allParam) {
+      FilterOrderDTO dto = objectMapper.convertValue(allParam, FilterOrderDTO.class);
+      if(dto.getId() != null) {
+        InProgressOrder inProgressOrder = this.inProgressOrderRepository.findByIdAndMerchantId(dto.getId(), dto.getMerchantId());
+        if(inProgressOrder == null) throw new BadRequestException("Order does not exist");
+        return ResponseEntity.ok(orderMapper.mapToResponse(inProgressOrder));
+      }
+      List<InProgressOrder> inProgressOrders = this.inProgressOrderRepository.filter(
+              dto.getMerchantId(),
+              dto.getStatuses(),
+              dto.getFromDate(),
+              dto.getToDate(),
+              PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "createdAt"))
+      );
+      List<OrderResponse> response = orderMapper.mapToResponse(inProgressOrders);
+      return ResponseEntity.ok(response);
     }
-    PushNotification pushNotification = new PushNotification();
-    pushNotification.setTitle(merchant.getName());
-    pushNotification.setBody("Đơn hàng mới được tạo");
-    notificationService.notiMerchant(merchant.getId(), pushNotification);
 
-    OrderResponse filterResponse = orderMapper.mapToResponse(savedInProgressOrder);
-    return ResponseEntity.ok(filterResponse);
-  }
+    @Transactional(propagation = Propagation.REQUIRED)
+    public ResponseEntity<OrderResponse> createOrder(CreateOrderDTO dto) throws ExecutionException, InterruptedException {
+      InProgressOrder savedInProgressOrder = this.create(dto);
+      productOrderService.bulkSave(dto.getProducts(), savedInProgressOrder);
+      Tables table = savedInProgressOrder.getTables();
+      Merchant merchant = savedInProgressOrder.getMerchant();
+      if(table != null ) {
+        table.setUsed(true);
+        this.tableRepository.save(table);
+      }
 
-  public ResponseEntity<Boolean> updateOrder(UpdateOrderDTO dto, Long orderId) throws ExecutionException, InterruptedException {
-    InProgressOrder inProgressOrder = this.inProgressOrderRepository.findByIdAndMerchantIdAndStatusIn(
-            orderId, dto.getMerchantId(), List.of(StatusOrder.WAITING, StatusOrder.PROGRESS)
-    );
-    if (inProgressOrder == null) throw new BadRequestException("Order does not exist");
-    modelMapper.map(dto, inProgressOrder);
-    this.inProgressOrderRepository.save(inProgressOrder);
+      PushNotification pushNotification = new PushNotification();
+      pushNotification.setTitle(merchant.getName());
+      pushNotification.setBody("Đơn hàng mới được tạo");
+      notificationService.notiMerchant(merchant.getId(), pushNotification);
 
-    CompletableFuture<Void> threads = CompletableFuture.allOf(dto.getProducts().stream().map((productOrderDTO) ->
-      CompletableFuture.runAsync(() -> {
-        ProductOrder productOrder = this.productOrderRepository.findByInProgressOrderIdAndProductId(inProgressOrder.getId(), productOrderDTO.getProductId());
-        if (productOrder != null) {
+      OrderResponse filterResponse = orderMapper.mapToResponse(savedInProgressOrder);
+      return ResponseEntity.ok(filterResponse);
+    }
+
+    public ResponseEntity<Boolean> updateOrder(UpdateOrderDTO dto, Long orderId) throws ExecutionException, InterruptedException {
+      InProgressOrder inProgressOrder = this.inProgressOrderRepository.findByIdAndMerchantIdAndStatusIn(
+              orderId, dto.getMerchantId(), List.of(StatusOrder.WAITING, StatusOrder.PROGRESS)
+      );
+      if (inProgressOrder == null) throw new BadRequestException("Order does not exist");
+      modelMapper.map(dto, inProgressOrder);
+      this.inProgressOrderRepository.save(inProgressOrder);
+
+      CompletableFuture<Void> threads = CompletableFuture.allOf(dto.getProducts().stream().map((productOrderDTO) ->
+        CompletableFuture.runAsync(() -> {
+          ProductOrder productOrder = this.productOrderRepository.findByInProgressOrderIdAndProductId(inProgressOrder.getId(), productOrderDTO.getProductId());
+          if (productOrder != null) {
+            modelMapper.map(productOrderDTO, productOrder);
+            this.productOrderRepository.save(productOrder);
+            return;
+          };
+          Product product = this.productRepository.findByIdAndMerchantId(productOrderDTO.getProductId(), dto.getMerchantId());
+          if (product == null) return;
+          productOrder = new ProductOrder();
+          productOrder.setInProgressOrder(inProgressOrder);
+          productOrder.setProduct(product);
           modelMapper.map(productOrderDTO, productOrder);
           this.productOrderRepository.save(productOrder);
-          return;
-        };
-        Product product = this.productRepository.findByIdAndMerchantId(productOrderDTO.getProductId(), dto.getMerchantId());
-        if (product == null) return;
-        productOrder = new ProductOrder();
-        productOrder.setInProgressOrder(inProgressOrder);
-        productOrder.setProduct(product);
-        modelMapper.map(productOrderDTO, productOrder);
-        this.productOrderRepository.save(productOrder);
-      }, asyncExecutor)
-    ).toArray(CompletableFuture[]::new));
-
-    threads.join();
-    return ResponseEntity.ok(true);
-  }
-
-  public ResponseEntity<Boolean> removeProductOrder(RemoveProductOrderDTO dto, Long id) throws ExecutionException, InterruptedException {
-    CompletableFuture<Void> threads = CompletableFuture.allOf( Arrays.stream(dto.getProducts()).map(productOrderDTO ->
-        CompletableFuture.runAsync(() -> {
-          this.productOrderRepository.remove(productOrderDTO.getProductId(), id);
         }, asyncExecutor)
-      ).toArray(CompletableFuture[]::new)
-    );
-    threads.join();
-    return ResponseEntity.ok(true);
-  }
+      ).toArray(CompletableFuture[]::new));
 
-  public ResponseEntity<Boolean> changeStatusOrder(Long orderId, ChangeStatusOrderDTO dto) {
-    InProgressOrder inProgressOrder = this.inProgressOrderRepository.findByIdAndMerchantId(orderId, dto.getMerchantId());
-    if (inProgressOrder == null) throw new BadRequestException("Order does not exist");
-    inProgressOrder.setStatus(dto.getStatus());
-    this.inProgressOrderRepository.save(inProgressOrder);
-    if((dto.getStatus().equals(StatusOrder.DONE) || dto.getStatus().equals(StatusOrder.CANCELED)) && inProgressOrder.getTables() != null) {
-      _resetTable(inProgressOrder.getTables());
+      threads.join();
+      return ResponseEntity.ok(true);
     }
-    return ResponseEntity.ok(true);
-  }
 
-  private void _resetTable(Tables table) {
-    table.setUsed(false);
-    this.tableRepository.save(table);
-  }
+    public ResponseEntity<Boolean> removeProductOrder(RemoveProductOrderDTO dto, Long id) throws ExecutionException, InterruptedException {
+      CompletableFuture<Void> threads = CompletableFuture.allOf( Arrays.stream(dto.getProducts()).map(productOrderDTO ->
+          CompletableFuture.runAsync(() -> {
+            this.productOrderRepository.remove(productOrderDTO.getProductId(), id);
+          }, asyncExecutor)
+        ).toArray(CompletableFuture[]::new)
+      );
+      threads.join();
+      return ResponseEntity.ok(true);
+    }
+
+    public ResponseEntity<Boolean> changeStatusOrder(Long orderId, ChangeStatusOrderDTO dto) {
+      InProgressOrder inProgressOrder = this.inProgressOrderRepository.findByIdAndMerchantId(orderId, dto.getMerchantId());
+      if (inProgressOrder == null) throw new BadRequestException("Order does not exist");
+      inProgressOrder.setStatus(dto.getStatus());
+      this.inProgressOrderRepository.save(inProgressOrder);
+      if((dto.getStatus().equals(StatusOrder.DONE) || dto.getStatus().equals(StatusOrder.CANCELED)) && inProgressOrder.getTables() != null) {
+        _resetTable(inProgressOrder.getTables());
+      }
+      return ResponseEntity.ok(true);
+    }
+
+    public InProgressOrder create(CreateOrderDTO dto) throws ExecutionException, InterruptedException {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      PodUserDetail userDetails = (PodUserDetail) authentication.getPrincipal();
+      CompletableFuture<Merchant> merchantRead = CompletableFuture.supplyAsync(() ->
+              merchantRepository.findByIdAndUserId(dto.getMerchantId(), userDetails.getId()), asyncExecutor
+      );
+      CompletableFuture<Users> userRead = CompletableFuture.supplyAsync(() ->
+                      userRepository.findById(userDetails.getId()).orElse(null),
+              asyncExecutor
+      );
+      CompletableFuture<Tables> tableRead = CompletableFuture.supplyAsync(() ->
+              dto.getTableId() == null ? null : tableRepository.findByIdAndMerchantIdAndIsUsedIsFalse(dto.getTableId(), dto.getMerchantId()), asyncExecutor
+      );
+      CompletableFuture<Void> allTasks = CompletableFuture.allOf(merchantRead, userRead, tableRead);
+      allTasks.join();
+
+      Merchant merchant = merchantRead.get();
+      if(merchant == null) throw new BadRequestException("Merchant not found");
+      Users user = userRead.get();
+      if(user == null) throw new BadRequestException("User not found");
+      Tables table = tableRead.get();
+
+      InProgressOrder inProgressOrder = modelMapper.map(dto, InProgressOrder.class);
+      inProgressOrder.setMerchant(merchant);
+      if(table != null)  inProgressOrder.setTables(table);;
+      inProgressOrder.setCreatedBy(user);
+
+      return inProgressOrderRepository.save(inProgressOrder);
+    }
+
+    private void _resetTable(Tables table) {
+      table.setUsed(false);
+      this.tableRepository.save(table);
+    }
 }
---
+++ src/main/java/org/example/podbackend/modules/orders/ProductOrderService.java
@@ -0,0 +1,47 @@
+package org.example.podbackend.modules.orders;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.example.podbackend.entities.InProgressOrder;
+import org.example.podbackend.entities.Product;
+import org.example.podbackend.entities.ProductOrder;
+import org.example.podbackend.modules.orders.DTO.ProductOrderDTO;
+import org.example.podbackend.repositories.ProductOrderRepository;
+import org.example.podbackend.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+@Service
+public class ProductOrderService {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  private final ProductOrderRepository productOrderRepository;
+  private final ProductRepository productRepository;
+  private final ModelMapper modelMapper;
+  public ProductOrderService(
+          ProductOrderRepository productOrderRepository,
+          ProductRepository productRepository,
+          ModelMapper modelMapper) {
+    this.productOrderRepository = productOrderRepository;
+    this.productRepository = productRepository;
+    this.modelMapper = modelMapper;
+  }
+
+  public void bulkSave(List<ProductOrderDTO> products, InProgressOrder inProgressOrder) {
+    products.forEach((productOrderDTO) -> {
+        Product product = this.productRepository.findByIdAndMerchantId(productOrderDTO.getProductId(), inProgressOrder.getMerchant().getId());
+        if (product == null) return;
+        ProductOrder productOrder = modelMapper.map(productOrderDTO, ProductOrder.class);
+        productOrder.setInProgressOrder(inProgressOrder);
+        productOrder.setProduct(product);
+        productOrderRepository.save(productOrder);
+      }
+    );
+  }
+}
